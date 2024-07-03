3c6acf37a2d7f73e715ea0da2bc6c4b0f1e5398e
MarekLempart <kama.zakupy@gmail.com>
2024-07-03 15:20:44 +0200

    Prace nad homework-04 - Schema

---
+++ homework-04/models/user.model.js
@@ -0,0 +1,29 @@
+// homework-04/models/user.model.js
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const user = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+const User = mongoose.model("user", user, "users");
+
+module.exports = User;
